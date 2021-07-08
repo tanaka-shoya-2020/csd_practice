c3cad631486f7e2d2d82da7e0501a1ef03d8e1b2
s.tanaka <shoya.t@gainsline.com>
2021-07-08 14:48:28 +0900

    add spell convert test

---
+++ src/SpellOutTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SpellOutTest {
+
+    @Test
+    public void SpellOut() {
+        SpellOut a = new SpellOut();
+        Map<Integer,String> map = new HashMap<>()
+        {{
+            put(90, "ninety nine" );
+            put(300, "three hundred");
+            put(310, "three hundred and ten");
+            put(1501, "one thousand, siz hundred and nine");
+            put(512607, "five hundred and twelve thousand, six hundred and seven");
+            put(43112603, "forty three million, one hundred and twelve thousand, six hundred and three");
+        }};
+        for(Map.Entry<Integer, String> entry : map.entrySet()){
+            assertEquals(entry.getValue(), a.convert(entry.getKey()));
+        }
+    }
+}
