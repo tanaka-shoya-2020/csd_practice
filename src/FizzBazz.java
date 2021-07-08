532244bec7cc561160449315e448f550e64d34e3
s.tanaka <shoya.t@gainsline.com>
2021-07-08 14:10:35 +0900

    add fizz bazz

--- src/FizzBazz.java
+++ src/FizzBazz.java
@@ -1,2 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class FizzBazz {
+    private int number = 100;
+    private int i;
+    private Map<Integer, String> map = new HashMap<>();
+    public Map<Integer, String> sayFizzBazz() {
+        for (i = 1; i <= number; i++) {
+            if (i % 15 == 0) {
+                map.put(i, "FizzBazz");
+            } else if (i % 3 == 0 || i % 10  == 3) {
+                map.put(i, "Fizz");
+            } else if (i % 5 == 0) {
+                map.put(i, "Bazz");
+            } else {
+                map.put(i, "");
+            }
+        }
+        return map;
+    }
 }
--- src/FizzBazzTest.java
+++ src/FizzBazzTest.java
@@ -2,20 +2,23 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBazzTest {
 
 	@Test
 	public void FizzBazz() {
-		fizzBazz = new FizzBazz();
+        FizzBazz a = new FizzBazz();
+		Map<Integer,String> fizzBazz = a.sayFizzBazz();
 		for(Map.Entry<Integer, String> entry : fizzBazz.entrySet()) {
 		  if (entry.getKey() % 15 == 0) {
-				assertThat(entry.getValue(), is("FizzBazz"));
-			} else if (entry.getKey() % 3 == 0 || entry.getKey() % 10 % 3 == 0) {
-				assertThat(entry.getValue(), is("Fizz"));
+				assertEquals("FizzBazz", entry.getValue());
+			} else if (entry.getKey() % 3 == 0 || entry.getKey() % 10  == 3) {
+		  		assertEquals("Fizz", entry.getValue());
 			} else if (entry.getKey() % 5 == 0) {
-			  	assertThat(entry.getValue(), is("Bazz"));
+			  	assertEquals("Bazz", entry.getValue());
 			} else {
-		  		assertThat(entry.getValue(), is(""));
+		  		assertEquals(entry.getValue(), "");
 			}
 		}
 	}
