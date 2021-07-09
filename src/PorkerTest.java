1111abb298051943a2508183fac7d9666685ebbe
s.tanaka <shoya.t@gainsline.com>
2021-07-09 12:09:18 +0900

    add pocker test

--- src/PorkerTest.java
+++ src/PorkerTest.java
@@ -9,13 +9,22 @@ public class PorkerTest {
     @Test
     public void Porker() {
         Porker a = new Porker();
-        Map<String,String> map = new HashMap<>()
+//        List<String> case1 = new ArrayList<>();
+//        Collections.addAll(case1,"2C", "3H", "4S", "8C", "AH" );
+
+        List<String> case2 = new ArrayList<>();
+        Collections.addAll(case2,"2H", "4S", "4C", "2D", "4H" );
+
+//        List<String> case3 = new ArrayList<>();
+//        Collections.addAll(case3,"2H", "3D", "5S", "9C", "KD" );
+
+        Map<List<String>,String> map = new HashMap<>()
         {{
-            put("2C 3H 4S 8C AH","high card: Ace");
-            put("2H 4S 4C 2D 4H","full house");
-            put("2H 3D 5S 9C KD","high card: 9");
+//            put(case1,"high card: Ace");
+            put(case2,"full house");
+//            put(case3,"high card: 9");
         }};
-        for(Map.Entry<String, String> entry : map.entrySet()){
+        for(Map.Entry<List<String>, String> entry : map.entrySet()){
             assertEquals(entry.getValue(), a.showDown(entry.getKey()));
         }
     }
